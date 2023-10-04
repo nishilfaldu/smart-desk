19a085fb092c9802ed2db2db65a5d6fcf9a84dd6
Zenith Zack <faldund@outlook.com>
2023-10-04 17:23:41 -0400

    feat(client): add shadcn-ui library configurations

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
 import { Nunito } from "next/font/google";
 
 
-import "@/styles/index.scss";
+import "@/styles/globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { PageHeader, PageHeaderDescription, PageHeaderHeading } from "@/components/PageHeader";
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { cn } from "@/lib/utils";
+import { ExamplesNav } from "@/components/ExamplesNav";
+import { buttonVariants } from "@/registry/default/ui/button";
 
 
 
@@ -20,9 +28,58 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${nunito.className}`}>
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <>
+            <div className="container relative">
+              <PageHeader className="page-header pb-8">
+                <Link
+                  href="/"
+                  className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium"
+                >
+                  🎉 <Separator className="mx-2 h-4" orientation="vertical" />{" "}
+                  <span className="sm:hidden">Style, a new CLI and more.</span>
+                  <span className="hidden sm:inline">
+                    Introducing Smarty, a new UI for a Smart Desk.
+                  </span>
+                  <ArrowRightIcon className="ml-1 h-4 w-4" />
+                </Link>
+                <PageHeaderHeading className="hidden md:block">
+                  Check out it's User Interface
+                </PageHeaderHeading>
+                <PageHeaderHeading className="md:hidden">Examples</PageHeaderHeading>
+                <PageHeaderDescription>
+                  Dashboard, settings, authentication, and much more.
+                </PageHeaderDescription>
+                <section className="flex w-full items-center space-x-4 pb-8 pt-4 md:pb-10">
+                  <Link
+                    href="/docs"
+                    className={cn(buttonVariants(), "rounded-[6px]")}
+                  >
+                    Get Started
+                  </Link>
+                  <Link
+                    href="/components"
+                    className={cn(
+                      buttonVariants({ variant: "outline" }),
+                      "rounded-[6px]"
+                    )}
+                  >
+                    Components
+                  </Link>
+                </section>
+              </PageHeader>
+              <section>
+                <ExamplesNav />
+                <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow">
+                  {children}
+                </div>
+              </section>
+            </div>
+          </>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
