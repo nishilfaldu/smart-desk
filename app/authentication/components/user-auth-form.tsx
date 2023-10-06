f26250eacd123c7058d21cb102b0755a3866e002
Zenith Zack <faldund@outlook.com>
2023-10-06 11:15:57 -0400

    feat(client): change styling of auth form

--- app/authentication/components/user-auth-form.tsx
+++ app/authentication/components/user-auth-form.tsx
@@ -49,11 +49,11 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
         </div>
       </form>
       <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
+        {/* <div className="absolute inset-0 flex items-center"> */}
+          {/* <span className="w-full border-t" /> */}
+        {/* </div> */}
         <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
+          <span className="bg-background px-2 ">
             Or continue with
           </span>
         </div>
--- app/authentication/page.tsx
+++ app/authentication/page.tsx
@@ -32,13 +32,13 @@ export default function AuthenticationPage() {
       </div>
       <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
-          href="/examples/authentication"
+          href="https://github.com/nishilfaldu/smart-desk"
           className={cn(
             buttonVariants({ variant: "ghost" }),
             "absolute right-4 top-4 md:right-8 md:top-8"
           )}
         >
-          Login
+          View Code
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
@@ -55,16 +55,16 @@ export default function AuthenticationPage() {
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
-            Acme Inc
+              Smarty Inc
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
-              <p className="text-lg">
+              {/* <p className="text-lg">
                 &ldquo;This library has saved me countless hours of work and
                 helped me deliver stunning designs to my clients faster than
                 ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
+              </p> */}
+              <footer className="text-sm">Nishil Faldu</footer>
             </blockquote>
           </div>
         </div>
