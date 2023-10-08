90b8360d86d80a38a464c3120d8cfcc6ae3ab28a
Zenith Zack <faldund@outlook.com>
2023-10-08 19:01:06 -0400

    feat(client): populate dropdown with existing users

--- app/dashboard/components/team-switcher.tsx
+++ app/dashboard/components/team-switcher.tsx
@@ -57,19 +57,6 @@ const groups = [
       },
     ],
   },
-  // {
-  //   label: "Teams",
-  //   teams: [
-  //     {
-  //       label: "Acme Inc.",
-  //       value: "acme-inc",
-  //     },
-  //     {
-  //       label: "Monsters Inc.",
-  //       value: "monsters",
-  //     },
-  //   ],
-  // },
 ]
 
 type Team = (typeof groups)[number]["teams"][number]
@@ -84,6 +71,41 @@ export default function TeamSwitcher({ className }: TeamSwitcherProps) {
   const [selectedTeam, setSelectedTeam] = React.useState<Team>(
     groups[0].teams[0]
   )
+  const [membersInfo, setMembersInfo] = React.useState<{ label: string; teams: any; }[]>([])
+
+  const getAllUsers = async () => {
+    try {
+      const apiUrl = "http://localhost:3000/api/get-all-users";
+      const response = await fetch(apiUrl);
+    if (response.ok) {
+      const result = await response.json();
+      const rows = result.result.rows;
+      console.log(rows);
+      const membersInfo = [
+        {
+          label: "Personal Account",
+          teams: rows.map((row) => ({
+            label: row.name,
+            value: "personal"
+          }))
+        },
+      ]
+      setMembersInfo(membersInfo);
+      console.log('Got all users successfully', result);
+      // Handle success, e.g., show a success message to the user
+    } else {
+      const errorData = await response.json();
+      console.error('Error getting all users:', errorData.error);
+      // Handle error, e.g., display an error message to the user
+    }
+    } catch(error) {
+      console.error('Error getting all users:', error);
+    }
+  }
+
+  React.useEffect(() => {
+    getAllUsers();
+  }, [])
 
   return (
     <Dialog open={showNewTeamDialog} onOpenChange={setShowNewTeamDialog}>
@@ -113,7 +135,7 @@ export default function TeamSwitcher({ className }: TeamSwitcherProps) {
             <CommandList>
               <CommandInput placeholder="Search user..." />
               <CommandEmpty>No user found.</CommandEmpty>
-              {groups.map((group) => (
+              {membersInfo.map((group) => (
                 <CommandGroup key={group.label} heading={group.label}>
                   {group.teams.map((team) => (
                     <CommandItem
